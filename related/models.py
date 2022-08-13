c371e58a836dd8466ff61eb233b7a527cfe718a4
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-14 02:09:25 +0430

    complete fields related to facility

--- related/models.py
+++ related/models.py
@@ -13,3 +13,17 @@ class relatedFacility(models.Model):
     type=models.CharField(max_length=20)
     disabled=models.BooleanField(default=False)
  
+class Field(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=20)
+    type=models.CharField(max_length=20)
+
+class relatedItemType(models.Model):
+    id=models.AutoField(primary_key=True)
+    active=models.BooleanField(default=True)
+    required=models.BooleanField(default=False)
+    disabled=models.BooleanField(default=False)
+    itemtype=models.ForeignKey(ItemType, on_delete=models.CASCADE)
+    field=models.ForeignKey(Field, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
