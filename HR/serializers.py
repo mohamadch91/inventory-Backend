65c63096573820688c4b1cc8651e83e11139f06c
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-18 04:12:29 +0430

    complete serializer

--- HR/serializers.py
+++ HR/serializers.py
@@ -1,13 +1,7 @@
 from rest_framework import serializers
 from .models import *
 
-# class helpSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Help
-#         fields = '__all__'
-#         # fields = ('id', 'title', 'code','active', 'created_at', 'updated_at')
-#         # extra_kwargs = {
-#         #     'created_at': {'read_only': True},
-#         #     'updated_at': {'read_only': True},
-#         # }
-
+class HRSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HR
+        fields = '__all__'
