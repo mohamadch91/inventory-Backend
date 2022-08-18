8ca1f1861a83149dfdf40feb60af77a1f1600b4f
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-19 03:31:48 +0430

    complete item serializer

--- item/serializers.py
+++ item/serializers.py
@@ -1,2 +1,7 @@
 from rest_framework import serializers
 from .models import *
+
+class itemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = item
+        fields = '__all__'
