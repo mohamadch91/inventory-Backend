4123f374eedd9f5ca45ebb350e375b58574a2e2d
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-18 05:34:23 +0430

    make migrations for message

--- message/models.py
+++ message/models.py
@@ -6,8 +6,8 @@ from facilities.models import Facility
 
 class message(models.Model):
     id=models.AutoField(primary_key=True)
-    sender=models.ForeignKey(Facility, on_delete=models.CASCADE, blank=True, null=True)
-    reciever=models.ForeignKey(Facility, on_delete=models.CASCADE, blank=True, null=True)
+    sender=models.ForeignKey(Facility, on_delete=models.CASCADE, blank=True, null=True,related_name='sender')
+    reciever=models.ForeignKey(Facility, on_delete=models.CASCADE, blank=True, null=True,related_name='reciever')
     subject=models.CharField(max_length=100,blank=True,null=True)
     body=models.TextField(max_length=500,blank=True,null=True)
     created_at=models.DateTimeField(auto_now_add=True)
