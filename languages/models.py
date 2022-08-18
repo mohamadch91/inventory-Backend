d80d1de7a5e129d8a6abbb1c6e82689fed806863
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-18 04:50:17 +0430

    complete language words model

--- languages/models.py
+++ languages/models.py
@@ -10,3 +10,14 @@ class languages(models.Model):
     updated_at=models.DateTimeField(auto_now=True)
     def __str__(self):
         return self.name
+
+class languages_words(models.Model):
+    id=models.AutoField(primary_key=True)
+    language=models.ForeignKey(languages, on_delete=models.CASCADE, blank=True, null=True)
+    word=models.CharField(max_length=100,blank=True,null=True)
+    translate=models.CharField(max_length=100,blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.word
+
