8c4850171d1775efb20ff69b89a6a065fc54b868
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-18 14:52:08 +0430

    add gender to HR

--- HR/models.py
+++ HR/models.py
@@ -3,10 +3,15 @@ from facilities.models import Facility
 # Create your models here.
 
 class HR (models.Model):
+    class gender(models.TextChoices):
+        male='male'
+        female='female'
+
     id=models.AutoField(primary_key=True)
     facility=models.ForeignKey(Facility, on_delete=models.CASCADE, blank=True, null=True)
     full_name=models.CharField(max_length=100,blank=True,null=True)
     position_level=models.CharField(max_length=100,blank=True,null=True)
+    gender=models.CharField(max_length=10,choices=gender.choices,blank=True,null=True)
     educatioin_level=models.CharField(max_length=100,blank=True,null=True)
     years_in_service=models.IntegerField(   blank=True,null=True)
     created_at=models.DateTimeField(auto_now_add=True)
