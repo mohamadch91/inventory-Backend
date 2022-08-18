60117f2f99da411becd3171970fd2b12b501702d
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-18 05:20:51 +0430

    complete serializer for message

--- message/serializers.py
+++ message/serializers.py
@@ -1,3 +1,8 @@
 from rest_framework import serializers
 from .models import *
 
+class messageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = message
+        fields = '__all__'
+        
