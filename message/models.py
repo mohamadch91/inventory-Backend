d050eb24d08ad00b69cc9e42b318f45fb95ca2bb
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-18 05:19:34 +0430

    complete message model

--- message/models.py
+++ message/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from facilities.models import Facility
 # Create your models here.
+
+# message model need sender reciever subject and body
+
+class message(models.Model):
+    id=models.AutoField(primary_key=True)
+    sender=models.ForeignKey(Facility, on_delete=models.CASCADE, blank=True, null=True)
+    reciever=models.ForeignKey(Facility, on_delete=models.CASCADE, blank=True, null=True)
+    subject=models.CharField(max_length=100,blank=True,null=True)
+    body=models.TextField(max_length=500,blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.subject
---
+++ message/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+
+    # path('', languageView.as_view(), name='language'),
+
+  
+    
+]
