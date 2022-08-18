0ef49b333f6fdb959f4fa2b79fd126c5c6bc07ac
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-18 04:48:31 +0430

    complete language model

--- languages/models.py
+++ languages/models.py
@@ -1,3 +1,12 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class languages(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=100,blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
