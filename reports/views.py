7945866da05d2a03092873aa15ec20ae54d784ec
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-25 15:48:11 +0430

    add view for reports

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from dataclasses import field
+from django.shortcuts import render
+
+# Create your views here.
+from re import I
+from django.shortcuts import render
+
+# Create your views here.
+import json
+from os import stat
+from urllib import response
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework.permissions import IsAuthenticated
+
+from authen.models import User
+from .models import *
+
+from items.serializers import *
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from items.models import ItemType
+from django.shortcuts import get_object_or_404
+
+class gapReportView(APIView):
+
+    def get(self,reqeust):
+        pass
+
+    
