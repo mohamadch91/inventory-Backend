147a366922b894be7c7738e72184d6aa6fc6fe94
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-29 02:59:20 +0430

    change maintance

--- maintanance/models.py
+++ maintanance/models.py
@@ -17,10 +17,20 @@ class maintanance(models.Model):
     requires=models.BooleanField(blank=True,null=True,default=True)
     freq_in_loc=models.IntegerField(blank=True,null=True)
     
-
+class maintancegp(models.Model):
+    id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=50,blank=True,null=True)
+    enable=models.BooleanField(default=True,blank=True,null=True)    
+    item_class=models.ForeignKey(ItemClass,models.CASCADE,null=True,blank=True)
+    item_type=models.ForeignKey(ItemType,models.CASCADE,null=True,blank=True)        
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
 class activeMaintance(models.Model):
     id=models.AutoField(primary_key=True)
     maintanance=models.ForeignKey(maintanance,models.CASCADE,blank=True,null=True)
+    maintanncegp=models.ForeignKey(maintanance,models.CASCADE,blank=True,null=True)
     enable=models.BooleanField(default=True,blank=True,null=True)            
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
