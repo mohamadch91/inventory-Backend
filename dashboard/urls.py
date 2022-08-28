2420cec6e75003b3523d29c9e3dc0b9e4b61a51d
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-29 02:33:54 +0430

    update dashboard tables

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
 
     path('item', dashboarditemView.as_view(), name='item'),
     path('facility', dashboardFacilityView.as_view(), name='facility'),
+    path('table', dahboardlevelView.as_view(), name='table'),
+
 
 
 
