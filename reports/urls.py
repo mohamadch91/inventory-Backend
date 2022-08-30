e369e5b1e84f8fe720bb75e1ab97b73d21a96aa9
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-08-30 05:41:47 +0430

    complete map based report

--- reports/urls.py
+++ reports/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
       path('excel', exportExcel.as_view(), name='export excel'),
       path('facseg', facilitysegView.as_view(), name='facility segmentation'),
       path('subfacpop', subfacView.as_view(), name='Sub Facility population data'),
-      path('facmap', subfacView.as_view(), name='facility map'),
+      path('facmap', facilitymap.as_view(), name='facility map'),
 
 
 
