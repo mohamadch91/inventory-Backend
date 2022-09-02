30eacf98d2c4636e8def30bdea3b000f78faee3f
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-09-02 05:09:57 +0430

    remove  serializer comments

--- reports/serializers.py
+++ reports/serializers.py
@@ -6,8 +6,3 @@ class gavSaveSerializer(serializers.ModelSerializer):
     class Meta:
         model = gapSave
         fields = '__all__'
-        # fields = ('id', 'title', 'code','active', 'created_at', 'updated_at')
-        # extra_kwargs = {
-        #     'created_at': {'read_only': True},
-        #     'updated_at': {'read_only': True},
-        # }
