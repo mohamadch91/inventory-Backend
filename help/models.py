8367e067459e51d2e55bd2bf128744ecf5426226
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-09-06 00:04:31 +0430

    lang fields

--- help/models.py
+++ help/models.py
@@ -4,22 +4,22 @@ from django.db import models
 # Create your models here.
 class Help(models.Model) :
     class choices(models.TextChoices):
-            dashboard = "Dashboard"  
-            facilities = "Facilities"  
-            hr = "Human Resources"  
-            user = "User"  
-            message = "Message"  
-            reports = "Reports"  
-            settings = "Settings"  
-            about_iga = "About-IGA"  
+            dashboard = "dashboard"  
+            facilities = "facilities"  
+            hr = "hr"  
+            user = "user"  
+            message = "message"  
+            reports = "reports"  
+            settings = "settings"  
+            about_iga = "about-iga"  
     class language(models.TextChoices):
         #arabic english persian french russian
-        en = "English"  
-        fr = "Français"  
-        ar = "العربية"  
-        fa = "فارسی"  
-        es = "Español"  
-        ru = "Русский"  
+        en = "en"  
+        fr = "fr"  
+        ar = "ar"  
+        fa = "fa"  
+        es = "es"  
+        ru = "ru"  
 
     page = models.CharField(max_length=20 , choices=choices.choices , blank=True , null=True)
     lang = models.CharField(max_length=20  ,choices=language.choices , blank=True , null=True)
