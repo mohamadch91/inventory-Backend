00335f6329f65b01ea601cd7d19ea1611c94b423
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-09-11 17:32:34 +0430

    change db again

--- inventory/settings.py
+++ inventory/settings.py
@@ -193,7 +193,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
-    "https://www.test-cors.org",'http://localhost:3000','https://invfrontdev.herokuapp.com','http://5.182.47.38:3002'
+    "https://www.test-cors.org",'http://localhost:3000','https://invfrontdev.herokuapp.com','http://5.182.47.38:3002','https://invgap.org'
 ]
 
 # CSRF_TRUSTED_ORIGINS = [
