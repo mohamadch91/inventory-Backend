1a6528ab2701e29b000ebc5d12a5401b4ab9562c
Mohamad Choupan <mohamadchoupan80@gmail.com>
2022-09-18 16:48:53 +0430

    change db

--- inventory/settings.py
+++ inventory/settings.py
@@ -139,22 +139,22 @@ WSGI_APPLICATION = 'inventory.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#   'default': {
-#      'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'inventorpft_db',
-#         'USER': 'postgres',
-#         'PASSWORD': 'tie9at9kpiue8de',
-#         'HOST': 'inventorydb-huj-service',
-#         'PORT': '',
-# }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
-    }
+  'default': {
+     'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'inventorpft_db',
+        'USER': 'postgres',
+        'PASSWORD': 'tie9at9kpiue8de',
+        'HOST': 'inventorydb-huj-service',
+        'PORT': '',
 }
+}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(BASE_DIR / 'db.sqlite3'),
+#     }
+# }
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
