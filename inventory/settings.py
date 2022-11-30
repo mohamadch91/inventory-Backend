e4ce9ee3aa86a75382813ba5b7a2f1d19104ae77
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-01 03:07:45 +0330

    feat: apply settings for UKR subdomain

--- inventory/settings.py
+++ inventory/settings.py
@@ -143,7 +143,7 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
 DATABASES = {
   'default': {
      'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'phi_db',
+        'NAME': 'ukr_db',
         'USER': 'postgres',
         'PASSWORD': 'fdhdkj@33443',
         'HOST': 'localhost',
