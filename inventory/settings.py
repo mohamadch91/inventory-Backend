90313396c47c4df27ed570082ce77599f657b275
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-15 19:05:12 +0330

    refactor: change db UKR

--- inventory/settings.py
+++ inventory/settings.py
@@ -143,7 +143,7 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
 DATABASES = {
   'default': {
      'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ukr_db',
+        'NAME': 'tls_db',
         'USER': 'postgres',
         'PASSWORD': 'fdhdkj@33443',
         'HOST': 'localhost',
