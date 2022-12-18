49c4a33546da8bf7c8615ef45fe93b0b32054c08
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-19 02:38:11 +0330

    fix: URL names

--- item/urls.py
+++ item/urls.py
@@ -8,9 +8,8 @@ urlpatterns = [
       path('item-field', itemFieldView.as_view(), name='item-field'),
      path('', itemView.as_view(), name='item'),   
      path('itempqs', itemPQSView.as_view(), name='item'),   
-     path('db', itemdb.as_view(), name='item'),   
-     path('dbfix', itemdbfix.as_view(), name='item'),   
-
+     path('db', itemdb.as_view(), name='item db'),   
+     path('dbfix', itemdbfix.as_view(), name='item db fix'),   
       path('delete', itemDeleteView.as_view(), name='item import with excel '),
       path('itemallFac', ItemAllfac.as_view(), name='item import with excel '),
       path('item-field-mobile', AllFieldView.as_view(), name='item import with excel '),
