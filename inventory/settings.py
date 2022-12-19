a6be14342ace47d8a6133cf8e54799256d76661b
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-19 08:09:27 +0330

    done

--- inventory/settings.py
+++ inventory/settings.py
@@ -140,16 +140,16 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-  'default': {
-     'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'tls_db',
-        'USER': 'postgres',
-        'PASSWORD': 'fdhdkj@33443',
-        'HOST': 'localhost',
-        'PORT': '',
-}
-}
+# DATABASES = {
+#   'default': {
+#      'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'tls_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'fdhdkj@33443',
+#         'HOST': 'localhost',
+#         'PORT': '',
+# }
+# }
 # DATABASES = {
 #   'default': {
 #      'ENGINE': 'django.db.backends.postgresql',
