18b74a8e9c5ec2412a68deb6186cff3586bd09c8
mohamadch91 <mohamadchoupan80@gmail.com>
2022-12-28 05:06:45 +0330

    feat: chanhe read_fix script for vietnam

--- read_fix.py
+++ read_fix.py
@@ -13,6 +13,7 @@ dic_arr_en=[]
 dic_arr_ar=[]
 dic_arr_ch=[]
 dic_arr_uk=[]
+dic_arr_vi=[]
 
 
 
@@ -24,10 +25,11 @@ for i in range(len(excel_data_df)):
     fr=excel_data_df['FR'][i]
     ar=excel_data_df['AR'][i]
     ru=excel_data_df['RU'][i]
-    sp=excel_data_df['SP'][i]
+    sp=excel_data_df['ES'][i]
     uk=excel_data_df['UK'][i]
     ot=excel_data_df['Other'][i]
     ch=excel_data_df['CH'][i]
+    vi=excel_data_df['VI'][i]
     if str(en)=='nan':
         en=cl
     if str(fr)=='nan':
@@ -37,13 +39,15 @@ for i in range(len(excel_data_df)):
     if str(ru)=='nan':
         ru='RU: '+cl
     if str(sp)=='nan':
-        sp='SP: '+cl
+        sp='ES: '+cl
     if str(ot)=='nan':
         ot='OT: '+cl
     if str(ch)=='nan':
         ch='CH: '+cl
     if str(uk)=='nan':
         uk='UK: '+cl
+    if str(vi)=='nan':
+        vi='VI: '+cl
     
     if ("'" in cl ):
         cl=cl.replace("'", "#")
@@ -63,13 +67,16 @@ for i in range(len(excel_data_df)):
         ch=ch.replace("'", "#")
     if ("'" in uk):
         uk=uk.replace("'", "#")
+    if ("'" in vi):
+        vi=vi.replace("'", "#")
+    
 
    
     data_en=  {
       "model": "languages.languages_words",
-      "pk": i*8+1,
+      "pk": i*9+1,
       "fields": {
-        "id":i*8+1,
+        "id":i*9+1,
         "language":1,
         "word":cl,
         "translate":en
@@ -79,9 +86,9 @@ for i in range(len(excel_data_df)):
     dic_arr_en.append(data_en)
     data_ar=  {
       "model": "languages.languages_words",
-      "pk": i*8+2,
+      "pk": i*9+2,
       "fields": {
-        "id":i*8+2,
+        "id":i*9+2,
         "language":2,
         "word":cl,
         "translate":ar
@@ -91,9 +98,9 @@ for i in range(len(excel_data_df)):
     dic_arr_ar.append(data_ar)
     data_fr=  {
       "model": "languages.languages_words",
-      "pk": i*8+3,
+      "pk": i*9+3,
       "fields": {
-        "id":i*8+3,
+        "id":i*9+3,
         "language":3,
         "word":cl,
         "translate":fr
@@ -103,9 +110,9 @@ for i in range(len(excel_data_df)):
     dic_arr_fr.append(data_fr)
     data_es=  {
       "model": "languages.languages_words",
-      "pk": i*8+4,
+      "pk": i*9+4,
       "fields": {
-        "id":i*8+4,
+        "id":i*9+4,
         "language":4,
         "word":cl,
         "translate":sp
@@ -115,9 +122,9 @@ for i in range(len(excel_data_df)):
     dic_arr_sp.append(data_es)
     data_fa=  {
       "model": "languages.languages_words",
-      "pk": i*8+5,
+      "pk": i*9+5,
       "fields": {
-        "id":i*8+5,
+        "id":i*9+5,
         "language":5,
         "word":cl,
         "translate":ot
@@ -127,9 +134,9 @@ for i in range(len(excel_data_df)):
     dic_arr_ot.append(data_fa)
     data_ru=  {
       "model": "languages.languages_words",
-      "pk": i*8+6,
+      "pk": i*9+6,
       "fields": {
-        "id":i*8+6,
+        "id":i*9+6,
         "language":6,
         "word":cl,
         "translate":ru
@@ -139,9 +146,9 @@ for i in range(len(excel_data_df)):
     dic_arr_ru.append(data_ru)
     data_uk=  {
       "model": "languages.languages_words",
-      "pk": i*8+7,
+      "pk": i*9+7,
       "fields": {
-        "id":i*8+7,
+        "id":i*9+7,
         "language":7,
         "word":cl,
         "translate":uk
@@ -151,9 +158,9 @@ for i in range(len(excel_data_df)):
     dic_arr_uk.append(data_uk)
     data_ch=  {
       "model": "languages.languages_words",
-      "pk": i*8+8,
+      "pk": i*9+8,
       "fields": {
-        "id":i*8+8,
+        "id":i*9+8,
         "language":8,
         "word":cl,
         "translate":ch
@@ -161,6 +168,18 @@ for i in range(len(excel_data_df)):
       }
     }
     dic_arr_ch.append(data_ch)
+    data_vi=  {
+      "model": "languages.languages_words",
+      "pk": i*9+9,
+      "fields": {
+        "id":i*9+9,
+        "language":9,
+        "word":cl,
+        "translate":vi
+        
+      }
+    }
+
 final=[]
 for a,b,c,d,e,f,g,h in zip(dic_arr_en,dic_arr_ar,dic_arr_fr,dic_arr_sp,dic_arr_ot,dic_arr_ru,dic_arr_ch,dic_arr_uk):
     
