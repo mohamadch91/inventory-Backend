84df9e452b784488ff6d6a8e276c7f5a408ace88
mohamadch91 <mohamadchoupan80@gmail.com>
2023-01-01 14:52:05 +0330

    feat: Apply changes for phl

--- inventory/settings.py
+++ inventory/settings.py
@@ -141,12 +141,12 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = 10240
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-runserver.default_port= (8002)
+runserver.default_port= (8003)
 runserver.default_addr = ("0.0.0.0")
 DATABASES = {
   'default': {
      'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ukr_db',
+        'NAME': 'phi_db',
         'USER': 'postgres',
         'PASSWORD': 'fdhdkj@33443',
         'HOST': 'localhost',
